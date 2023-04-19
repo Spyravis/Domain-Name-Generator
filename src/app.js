9dcfb60b9814519a526881057ee376fdf04ba073
Spyravis <ritalopezcobo@gmail.com>
2023-04-19 06:21:09 +0000

    done

--- src/app.js
+++ src/app.js
@@ -7,5 +7,25 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
+};
+
+let generateExcuse = () => {
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return who[whoIndx] + " " + what[whatIndx] + " " + when[whenIndx] + " ";
 };
